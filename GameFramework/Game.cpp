8dd76b7df994c44b6a9a64d648e1e09cb839f523
이성규 <seong9945@naver.com>
2018-11-18 01:59:00 +0900

    11월 18일 PP05.AnimatingSprite

--- GameFramework/Game.cpp
+++ GameFramework/Game.cpp
@@ -5,7 +5,7 @@ bool Game::init(const char*title, int xpos, int ypos, int width,
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		m_pWindow = SDL_CreateWindow("PP04.BasicDrawing",
+		m_pWindow = SDL_CreateWindow("PP05.AnimatingSprite",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
 			640, 480, SDL_WINDOW_SHOWN);
@@ -15,11 +15,17 @@ bool Game::init(const char*title, int xpos, int ypos, int width,
 
 		m_bRunning = true;
 
-		SDL_Surface*pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+		SDL_Surface*pTempSurface = SDL_LoadBMP("assets/animate.bmp");
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);
-		SDL_QueryTexture(m_pTexture, NULL, NULL,
-			&m_sourceRectangle.w, &m_sourceRectangle.h);
+
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
 	}
 	else {
 		return false;
@@ -29,11 +35,6 @@ bool Game::init(const char*title, int xpos, int ypos, int width,
 
 void Game::render()
 {
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
-
 	SDL_RenderClear(m_pRenderer);
 	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
@@ -41,6 +42,7 @@ void Game::render()
 
 void Game::update()
 {
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
 
 void Game::clean()
